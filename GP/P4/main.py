3e92f71a3fd7c867b694d967bbf24ee35c0103d1
parse112233 <parse11223390@gmail.com>
2024-07-24 09:06:54 +0530

    added p4 in gp pracs

---
+++ GP/P4/main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import math
+root = Tk()
+
+C = Canvas(root, bg="white", height=850, width=850)
+
+a1, a2 = 126, 154
+b1, b2 = 395, 154
+c1, c2 = 395, 454
+d1, d2 = 126, 454
+
+C.create_polygon( a1,a2,b1,b2,c1,c2,d1,d2,fill="blue")
+
+
+xsh1 = a1 + a2 * math.tan(math.radians(45))
+xsh2 = d1 + d2 * math.tan(math.radians(45))
+
+C.create_polygon( xsh1,a2,xsh2,b2,c1,c2,d1,d2,fill="red")
+
+C.pack()
+mainloop()
